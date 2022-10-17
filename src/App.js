4d56d94307cdb65450b49ebdb8790e30c9da7126
jyoti01939 <jgupta96353@gmail.com>
2022-10-17 18:58:41 +0530

    useState enable dark mode

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import './App.css';
-import Navbar from './components/Navbar';
-import Textform from './components/Textform';
+import "./App.css";
+// import About from "./components/About";
+import Navbar from "./components/Navbar";
+import Textform from "./components/Textform";
 
 function App() {
   return (
     <>
-      <Navbar title="My App" aboutText="About MyApp"/>
+      <Navbar title="My App" aboutText="About MyApp" />
       <div className="container my-5">
-      <Textform heading="Enter The Text To Analyise"/>
+        <Textform heading="Enter The Text To Analyise" />
+      {/* <About/> */}
       </div>
-      
     </>
-    
   );
 }
 
--- src/components/Textform.js
+++ src/components/Textform.js
@@ -1,23 +1,69 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 
 export default function Textform(props) {
-    const handleUpClick = ()=>{
-        // console.log("Uppercase was Clicked " + text);
-        let newText = text.toUpperCase();
-        setText(newText);
-    }
-    const handleOnChange = (event)=>{
-        // console.log("Uppercase was Changed");
-        setText(event.target.value);
-    }
-    const [text, setText] = useState("Enter Text Here");
+  const handleUpClick = () => {
+    // console.log("Uppercase was Clicked " + text);
+    let newText = text.toUpperCase();
+    setText(newText);
+  };
+
+  const handleLoClick = () => {
+    let newText = text.toLowerCase();
+    setText(newText);
+  };
+
+  const handleClearClick = () => {
+    let newText = "";
+    setText(newText);
+  };
+
+  const handleOnChange = (event) => {
+    // console.log("Uppercase was Changed");
+    setText(event.target.value);
+  };
+  const [text, setText] = useState("");
   return (
-    <div>
+    <>
+      <div className="container">
         <h3>{props.heading}</h3>
-      <div className="mb-3">
-        <textarea className="form-control" id="myBox" rows="8" value={text} onChange={handleOnChange}></textarea>
-    </div>
-    <button className="btn btn-primary btn-sm rounded-pill" onClick={handleUpClick}>Change to uppercase</button>
-    </div>
-  )
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            id="myBox"
+            rows="8"
+            value={text}
+            onChange={handleOnChange}
+          ></textarea>
+        </div>
+        <button
+          className="btn btn-primary btn-sm rounded-pill mx-1"
+          onClick={handleUpClick}
+        >
+          Change to uppercase
+        </button>
+        <button
+          className="btn btn-primary btn-sm rounded-pill mx-1"
+          onClick={handleLoClick}
+        >
+          Change to lowercase
+        </button>
+        <button
+          className="btn btn-primary btn-sm rounded-pill mx-1"
+          onClick={handleClearClick}
+        >
+          Clear Text
+        </button>
+      </div>
+
+      <div className="container my-5">
+        <h3>Your Text Here</h3>
+        <p>
+          {text.split(" ").length} words {text.length} characters
+        </p>
+        <p>{0.008 * text.split(" ").length} Mintues to read</p>
+        <h4>Preview</h4>
+        <p>{text}</p>
+      </div>
+    </>
+  );
 }
